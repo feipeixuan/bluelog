572ee72f0ae06cb01934f8a0b756047d6ffeb586
feipeixuan <feipeixuan@163.com>
2020-04-01 12:20:46 +0800

    增加限制条件

--- bluelog/blueprints/blog.py
+++ bluelog/blueprints/blog.py
@@ -21,7 +21,12 @@ blog_bp = Blueprint('blog', __name__)
 def index():
     page = request.args.get('page', 1, type=int)
     per_page = current_app.config['BLUELOG_POST_PER_PAGE']
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=per_page)
+    # TODO 如果包含session
+    if current_user.is_authenticated:
+        pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=per_page)
+    else:
+        pagination = Post.query.filter(is_private=False).order_by(Post.timestamp.desc()).paginate(page, per_page=per_page)
+
     posts = pagination.items
     return render_template('blog/index.html', pagination=pagination, posts=posts)
 
