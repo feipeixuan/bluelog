7159b07a40b5556aa865340fd37941dd00f3e42d
feipeixuan <feipeixuan@163.com>
2020-04-04 22:23:45 +0800

    fix bug

--- bluelog/models.py
+++ bluelog/models.py
@@ -87,4 +87,4 @@ class Link(db.Model):
 class Idea(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(30), nullable=False)
-    progress = db.Column(db.Integer, default=0)
+    progress = db.Column(db.Integer, default=0,nullable=False)
